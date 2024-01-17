a4f70acbdc8c965fa51c5dc02fe5d9d3563858fa
Tung Nguyen <thanhtungnguyen9911@gmail.com>
2024-01-17 09:44:07 +0700

    fix something

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -11,6 +11,7 @@ import Typography from '@mui/material/Typography';
 import ContactUser from './ui/contactUser/page';
 import OwnerData from './ui/ownerData/page';
 import Payment from './ui/payment/page';
+import MainLayout from "@/layouts/MainLayout";
 
 const steps = ['Basic Information', 'Owner Data', 'Payment Information'];
 
@@ -59,60 +60,62 @@ export default function Contact() {
     };
 
     return (
-        <Box className="form_background" sx={{ width: '40%' }}>
-            <p className="frame_banner"></p>
-            <div >
-                {allStepsCompleted() ? (
-                    <React.Fragment>
-                        <Typography sx={{ mt: 1 }}>
-                            THANKS YOU
-                        </Typography>
-                        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                            <Box sx={{ flex: '1 1 auto' }} />
-                        </Box>
-                        <Button onClick={handleReset}>Reset</Button>
-                    </React.Fragment>
-                ) : (
-                    <React.Fragment>
-                        <div className="form_contact  mt: 3, mb: 1, py: 1"  >
-                            <div className="form">
-                                <div className="basic_information">
-                                    <a href="#">Basic Information</a>
+        <MainLayout>
+            <Box className="form_background" sx={{ width: '40%' }}>
+                <p className="frame_banner"></p>
+                <div >
+                    {allStepsCompleted() ? (
+                        <React.Fragment>
+                            <Typography sx={{ mt: 1 }}>
+                                THANKS YOU
+                            </Typography>
+                            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                                <Box sx={{ flex: '1 1 auto' }} />
+                            </Box>
+                            <Button onClick={handleReset}>Reset</Button>
+                        </React.Fragment>
+                    ) : (
+                        <React.Fragment>
+                            <div className="form_contact  mt: 3, mb: 1, py: 1"  >
+                                <div className="form">
+                                    <div className="basic_information">
+                                        <a href="#">Basic Information</a>
+                                    </div>
+                                    <Stepper nonLinear activeStep={activeStep}>
+                                        {steps.map((label, index) => (
+                                            <Step key={label} completed={completed[index]}>
+                                                <StepButton color="inherit">
+                                                    {label}
+                                                </StepButton>
+                                            </Step>
+                                        ))}
+                                    </Stepper>
+                                    {activeStep === 0 && <ContactUser />}
+                                    {activeStep === 1 && <OwnerData />}
+                                    {activeStep === 2 && <Payment />}
+    
+                                    <Box className='form_buttom' sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                                        <Box sx={{ flex: '1 1 auto' }} />
+                                        {activeStep !== steps.length &&
+                                            (completed[activeStep] ? (
+                                                <Typography variant="caption" sx={{ display: 'inline-block' }}>
+                                                </Typography>
+                                            ) :
+                                                <Button className='text_button' onClick={handleComplete}>
+                                                    {completedSteps() === totalSteps() - 1
+                                                        ? 'Continue'
+                                                        : 'Continue'}
+                                                </Button>
+                                            )}
+                                    </Box>
+    
                                 </div>
-                                <Stepper nonLinear activeStep={activeStep}>
-                                    {steps.map((label, index) => (
-                                        <Step key={label} completed={completed[index]}>
-                                            <StepButton color="inherit">
-                                                {label}
-                                            </StepButton>
-                                        </Step>
-                                    ))}
-                                </Stepper>
-                                {activeStep === 0 && <ContactUser />}
-                                {activeStep === 1 && <OwnerData />}
-                                {activeStep === 2 && <Payment />}
-
-                                <Box className='form_buttom' sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                                    <Box sx={{ flex: '1 1 auto' }} />
-                                    {activeStep !== steps.length &&
-                                        (completed[activeStep] ? (
-                                            <Typography variant="caption" sx={{ display: 'inline-block' }}>
-                                            </Typography>
-                                        ) :
-                                            <Button className='text_button' onClick={handleComplete}>
-                                                {completedSteps() === totalSteps() - 1
-                                                    ? 'Continue'
-                                                    : 'Continue'}
-                                            </Button>
-                                        )}
-                                </Box>
-
+    
                             </div>
-
-                        </div>
-                    </React.Fragment>
-                )}
-            </div>
-        </Box>
+                        </React.Fragment>
+                    )}
+                </div>
+            </Box>
+        </MainLayout>
     )
 }
